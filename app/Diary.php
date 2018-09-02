fad1d4479fe6efd87a98722a3602add893b48c60
shirasu <shira7867@gmail.com>
2018-09-03 00:32:02 +0900

    create diary model

---
+++ app/Diary.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Diary extends Model
+{
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'date' => 'required|date',
+        'content' => 'required|string', 
+    );
+}
